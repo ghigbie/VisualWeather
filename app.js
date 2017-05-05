d53179c8be7149624e57f942c6c3fa92ebeb365f
George Higbie <gehigbie@gmail.com>
2017-05-04 17:40:08 -0700

    Add not found route

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 3020;
 
 app.set("view engine", "ejs");
 
+app.get("*", (req, res) => {
+    res.render("notfound");
+});
+
 app.listen(port, process.env.IP, () => {
     console.log(`Server is up on port ${port}`);
 });
