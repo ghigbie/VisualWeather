9714750f6cd3cc6b49fade86e732d7cd26641018
George Higbie <gehigbie@gmail.com>
2017-05-04 17:38:46 -0700

    Change port

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require(express),
-          ejs = require(ejs),
+const express = require("express"),
+          ejs = require("ejs"),
           app = express();
 
-const port = process.env.PORT || 3000,
-        IP = process.env.IP;
+const port = process.env.PORT || 3020;
 
-app.listen(port, IP, () => {
-    console.log(`Server is up on port ${port}`)
-})
+app.set("view engine", "ejs");
+
+app.listen(port, process.env.IP, () => {
+    console.log(`Server is up on port ${port}`);
+});
